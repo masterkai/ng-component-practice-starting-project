7d0ce7843ae505fd1db85400fac0c7c46af3e50c
max liu <max.liu@digitimes.com>
2024-09-02 17:53:47 +0800

    feat(server-status): Add server status component (main)
    Adds a server status component that displays the current status of the servers.
    The component updates the status every 5 seconds with a random status of "online", "offline", or "unknown".
    
    The component uses conditional classes to style the status based on the current status.
    
    The status is updated every 5 seconds with a random status of "online", "offline", or "unknown".

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 type Status = 'online' | 'offline' | 'unknown';
 @Component({
   selector: 'app-server-status',
@@ -7,11 +13,13 @@ type Status = 'online' | 'offline' | 'unknown';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  // private interval?: ReturnType<typeof setInterval>;
   currentStatus: Status = 'online';
-
-  constructor() {
-    setInterval(() => {
+  constructor() {}
+  ngOnInit() {
+    const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5) {
         this.currentStatus = 'online';
@@ -21,5 +29,13 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
+
+  // ngOnDestroy(): void {
+  //   clearInterval(this.interval);
+  // }
 }
