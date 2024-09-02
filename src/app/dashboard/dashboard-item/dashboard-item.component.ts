68a45a359a230af4478b24bdcd826dedeb9786a3
max liu <max.liu@digitimes.com>
2024-09-02 10:02:08 +0800

    first commit

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,18 @@
+import {Component, input} from '@angular/core';
+
+export interface Image {
+  src: string;
+  alt: string;
+}
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+  image = input.required<Image>()
+  title = input.required<string>()
+}
